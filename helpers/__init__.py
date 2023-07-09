47b63a2bb71863c2b9f60b97ad7ff7caba4b4b96
Robin <50741530+Alpha62579@users.noreply.github.com>
2023-07-09 12:35:06 +0530

    Update VERSION

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -6,7 +6,7 @@ A collection of helper functions for my stupid CS project at school
 
 __name__ = "CBSEHelpers"
 __author__ = "Alpha62579"
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __license__ = "MIT"
 
 from . import menu as menu
