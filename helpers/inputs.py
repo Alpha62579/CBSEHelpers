24c21ec83e19d77c5b279cb27d5ed2764cc81126
Alpha62579 <50741530+Alpha62579@users.noreply.github.com>
2023-08-17 14:21:22 +0530

    Added support for custom input functions.

--- helpers/inputs.py
+++ helpers/inputs.py
@@ -8,17 +8,18 @@ from typing import Optional, Callable
 
 
 def get_int(prompt: str, /, *, error_str: Optional[str] = "Please enter a valid input.",
-            check: Callable[[int], bool] = lambda x: True) -> int:
+            check: Callable[[int], bool] = lambda x: True, cinput: Callable[[str], str] = input) -> int:
     """
     Gets an integer from the user.
     :param prompt: The prompt to display to the user.
     :param error_str: The error message to display to the user. Defaults to "Please enter a valid input."
     :param check: The function to check the input against. The callable must accept one argument and return a boolean.
+    :param cinput: The function to use for getting input. Defaults to input.
     :return: int
     """
     while True:
         try:
-            value = int(input(prompt))
+            value = int(cinput(prompt))
             if check(value):
                 return value
             else:
@@ -28,17 +29,18 @@ def get_int(prompt: str, /, *, error_str: Optional[str] = "Please enter a valid 
 
 
 def get_float(prompt: str, /, *, error_str: Optional[str] = "Please enter a valid input.",
-              check: Callable[[float], bool] = lambda x: True) -> float:
+              check: Callable[[float], bool] = lambda x: True, cinput: Callable[[str], str] = input) -> float:
     """
     Gets a float from the user.
     :param prompt: The prompt to display to the user.
     :param error_str: The error message to display to the user. Defaults to "Please enter a valid input."
     :param check: The function to check the input against. The callable must accept one argument and return a boolean.
+    :param cinput: The function to use for getting input. Defaults to input.
     :return: float
     """
     while True:
         try:
-            value = float(input(prompt))
+            value = float(cinput(prompt))
             if check(value):
                 return value
             else:
@@ -47,16 +49,17 @@ def get_float(prompt: str, /, *, error_str: Optional[str] = "Please enter a vali
             sys.stderr.write(error_str)
 
 
-def get_bool(prompt: str, /, *, error_str: Optional[str] = "Please enter a valid input.") -> bool:
+def get_bool(prompt: str, /, *, error_str: Optional[str] = "Please enter a valid input.", cinput: Callable[[str], str] = input) -> bool:
     """
     Gets a boolean from the user.
     This supports the usage of y/n, yes/no, true/false, and 1/0.
     :param prompt: The prompt to display to the user.
     :param error_str: The error message to display to the user. Defaults to "Please enter a valid input."
+    :param cinput: The function to use for getting input. Defaults to input.
     :return: bool
     """
     while True:
-        value = input(prompt).lower().strip()
+        value = cinput(prompt).lower().strip()
         if value in difflib.get_close_matches(value, ('yes', 'no', 'true', 'false', '1', '0')):
             if value in ('yes', 'true', '1'):
                 return True
@@ -67,16 +70,17 @@ def get_bool(prompt: str, /, *, error_str: Optional[str] = "Please enter a valid
 
 
 def get_str(prompt: str, /, *, error_str: Optional[str] = "Please enter a valid input.",
-            check: Callable[[str], bool] = lambda x: True) -> str:
+            check: Callable[[str], bool] = lambda x: True, cinput: Callable[[str], str] = input) -> str:
     """
     Gets a string from the user.
     :param prompt: The prompt to display to the user.
     :param error_str: The error message to display to the user. Defaults to "Please enter a valid input."
     :param check: The function to check the input against. The callable must accept one argument and return a boolean.
+    :param cinput: The function to use for getting input. Defaults to input.
     :return: str
     """
     while True:
-        value = input(prompt).strip()
+        value = cinput(prompt).strip()
         if check(value):
             return value
         else:
